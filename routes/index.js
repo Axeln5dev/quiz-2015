0a1c142a72b6d8769eb4071a082475656eb77aeb
Alejandro campos <axeln5@hotmail.com>
2015-06-17 21:24:16 +0200

    Plantilla
    Estructura inicial

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Quiz 2015' });
+});
+
+module.exports = router;
