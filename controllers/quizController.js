5acc7631ff829c34f0dafc5fdbefe66099c0becf
Alejandro campos <axeln5@hotmail.com>
2015-06-18 19:20:40 +0200

    Quiz controller y favicon
    Se realizan los desarrollos necesario para incluir un controller de
    preguntas y respuestas, y el favicon de la pagina

---
+++ controllers/quizController.js
@@ -0,0 +1,29 @@
+/*
+ * Controlador para las peticiones relaciondas con las preguntas y respuestas del juego QUIZ 2015
+ */
+
+// Metodo manejador de la peticion GET '/question'
+exports.question = function(req, res) {
+  console.log('en quesion');
+  res.render('quizes/question', {
+    'quiz_question': 'Cual es la capital de Italia?'
+  });
+};
+
+// Metodo manejador de la peticion GET '/answer'
+exports.answer = function(req, res) {
+  var message = '';
+  var answerOk = 'Roma';
+
+  // Evaluamos la respuesta y componemos el mensaje con el que responder
+  if (req.query.answer.toLowerCase() === answerOk.toLowerCase()) {
+    message = 'Enhorabuena!! la respuesta ' + answerOk + ' es correcta.';
+  } else {
+    message = 'Ooohhh!! la respuesta es incorrecta. La respuesta correcta es ' + answerOk;
+  }
+
+  // Enviamos respuesta a la vista
+  res.render('quizes/answer', {
+    'message': message
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+// Importamos el Controlador de quizes quizController
+var quizController = require('../controllers/quizController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz 2015' });
 });
+
+// Definicion de rutas que manejara quizController
+router.get('/question', quizController.question);
+router.get('/answer', quizController.answer);
 
 module.exports = router;
