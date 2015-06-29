28d7457fd18ac6b5535a8b75cf6fefb8eece7c7b
Alejandro campos <axeln5@hotmail.com>
2015-06-29 21:13:57 +0200

    Desarrollo pagina de creditos
    Se realizan los desarrollos necesarios para mostrar los autores de la
    pagina

---
+++ controllers/authorController.js
@@ -0,0 +1,10 @@
+// Metodo manejador de la peticion GET '/question'
+exports.author = function(req, res) {
+  res.render('author/author', {
+    'author': {
+        name: 'Alejandro',
+        image: '/images/photo.png',
+        description: 'Soy el creador de esta web!!'
+      }
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 // Importamos el Controlador de quizes quizController
 var quizController = require('../controllers/quizController');
+// Importamos el Controlador de autores de la pagina
+var authorController = require('../controllers/authorController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,5 +14,8 @@ router.get('/', function(req, res, next) {
 // Definicion de rutas que manejara quizController
 router.get('/question', quizController.question);
 router.get('/answer', quizController.answer);
+
+// Definicion de rutas que maneja los creadores de la pagina
+router.get('/author', authorController.author);
 
 module.exports = router;
