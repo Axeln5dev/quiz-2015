0eca3b188bbbadc6505bb223e40e82af8e55b7bd
Alejandro campos <axeln5@hotmail.com>
2015-07-25 19:13:48 +0200

    Corrección de bug en la pagina de creditos

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,11 +1,12 @@
 // Metodo manejador de la peticion GET '/question'
 exports.author = function(req, res) {
+  console.log('asdasdasdasdas');
   res.render('author/author', {
     'author': {
         name: 'Alejandro',
         image: '/images/photo.png',
-        description: 'Soy el creador de esta web!!',
-        errors: []
-      }
+        description: 'Soy el creador de esta web!!'
+      },
+    errors: []
   });
 };
